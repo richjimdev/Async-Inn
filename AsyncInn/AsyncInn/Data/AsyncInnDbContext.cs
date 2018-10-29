51bff5813a242e7bca16b38457fdabd2130e93dd
Richard J <richjim89@gmail.com>
2018-10-28 23:40:18 -0700

    database seed

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -23,6 +23,113 @@ namespace AsyncInn.Data
             modelBuilder.Entity<RoomAmenities>().HasKey(
                 ce => new { ce.AmenitiesID, ce.RoomID }
                 );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    ID = 1,
+                    Name = "Async Seattle",
+                    Address = "12 Async Way, Seattle, WA 91234",
+                    Phone = "206-123-1234"
+                },
+                new Hotel
+                {
+                    ID = 2,
+                    Name = "Async NYC",
+                    Address = "12 Async Way, New York City, NY 91234",
+                    Phone = "555-123-1234"
+                },
+                new Hotel
+                {
+                    ID = 3,
+                    Name = "Async LA",
+                    Address = "12 Async Way, Los Angeles, CA 91234",
+                    Phone = "555-123-1234"
+                },
+                new Hotel
+                {
+                    ID = 4,
+                    Name = "Async Portland",
+                    Address = "12 Async Way, Portland, OR 91234",
+                    Phone = "555-123-1234"
+                },
+                new Hotel
+                {
+                    ID = 4,
+                    Name = "Async Honolulu",
+                    Address = "12 Async Way, Honolulu, HI 91234",
+                    Phone = "555-123-1234"
+                }
+                );
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    ID = 1,
+                    Name = "Sleeping Orca",
+                    Layout = Layout.OneBedroom
+                },
+                new Room
+                {
+                    ID = 2,
+                    Name = "Snoring Grizzly",
+                    Layout = Layout.Studio
+                },
+                new Room
+                {
+                    ID = 3,
+                    Name = "Snozing Dolphin",
+                    Layout = Layout.TwoBedroom
+                },
+                new Room
+                {
+                    ID = 4,
+                    Name = "Lazy Sloth",
+                    Layout = Layout.OneBedroom
+                },
+                new Room
+                {
+                    ID = 5,
+                    Name = "Sleepy Panda",
+                    Layout = Layout.TwoBedroom
+                },
+                new Room
+                {
+                    ID = 6,
+                    Name = "Yawning Peacock",
+                    Layout = Layout.Studio
+                }
+
+                );
+
+            modelBuilder.Entity<Room>().HasData(
+                new Amenities
+                {
+                    ID = 1,
+                    Name = "Coffee Machine"
+                },
+                new Amenities
+                {
+                    ID = 2,
+                    Name = "Flat Screen TV"
+                },
+                new Amenities
+                {
+                    ID = 3,
+                    Name = "Hot Tub"
+                },
+                new Amenities
+                {
+                    ID = 4,
+                    Name = "A/C"
+                },
+                new Amenities
+                {
+                    ID = 5,
+                    Name = "Toaster"
+                }
+
+                );
         }
 
         public DbSet<Amenities> Amenities { get; set; }

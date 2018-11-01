d75038a9ee33866d3d2901c11ad2beb29f1d2e43
Richard J <richjim89@gmail.com>
2018-11-01 11:55:34 -0700

    Services created

---
+++ AsyncInn/AsyncInn/Models/Services/AmenitiesService.cs
@@ -0,0 +1,46 @@
+﻿using AsyncInn.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AsyncInn.Data;
+
+namespace AsyncInn.Models.Services
+{
+    public class AmenitiesService : IAmenities
+    {
+        private AsyncInnDbContext _context;
+
+        public AmenitiesService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAmenity(Amenities amenity)
+        {
+            _context.Amenities.Add(amenity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAmenities(int id)
+        {
+            Amenities amenity = await GetAmenity(id);
+            _context.Amenities.Remove(amenity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<Amenities>> GetAmenities()
+        {
+            return await _context.Amenities.ToListAsync();
+        }
+        public async Task<Amenities> GetAmenity(int? id)
+        {
+            return await _context.Amenities.FirstOrDefaultAsync(x => x.ID == id);
+        }
+        public async Task UpdateAmenities(Amenities amenity)
+        {
+            _context.Amenities.Update(amenity);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
